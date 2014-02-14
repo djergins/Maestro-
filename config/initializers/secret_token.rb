aa92dfaa7f0bbd2f133d969457e1d3d0aece693a
Daniel Jergins <danieljergins@gmail.com>
2014-02-14 15:18:24 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaestroWebapp::Application.config.secret_key_base = '31f7acf448fa67605c65782def4782920658e1bd9c02fc04c61b723e0d96799800452b007bff6e3568b9730ec09b162f24f7c0b8953a71f40d28964b5db0735a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MaestroWebapp::Application.config.session_store :cookie_store, key: '_maestro_webapp_session'
